98733c6d371e3399e5c8d83652cf48ba2a729865
mikelisrael <israelipinkz@gmail.com>
2025-08-14 09:11:35 +0100

    feat: Enhance Profile screen layout with SafeAreaView and icon integration

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,16 @@
-import React from "react"
-import { Text, View } from "react-native"
+import Icon from "@react-native-vector-icons/ionicons";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1 bg-primary px-10">
+      <View className="flex flex-1 flex-col items-center justify-center gap-5">
+        <Icon name="person-circle-outline" size={40} color="#fff" />
+        <Text className="text-base text-gray-500">Profile</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Profile
+export default Profile;
