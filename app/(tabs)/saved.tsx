df4270b142a8f12a457e6bb52b744f14bce9db3b
mikelisrael <israelipinkz@gmail.com>
2025-08-14 09:11:45 +0100

    feat: Refactor Saved screen to use SafeAreaView and enhance layout with icons

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,12 +1,16 @@
-import React from "react"
-import { Text, View } from "react-native"
+import Icon from "@react-native-vector-icons/ionicons";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1 bg-primary px-10">
+      <View className="flex flex-1 flex-col items-center justify-center gap-5">
+        <Icon name="book-outline" size={40} color="#fff" />
+        <Text className="text-base text-gray-500">Saved</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Saved
+export default Saved;
